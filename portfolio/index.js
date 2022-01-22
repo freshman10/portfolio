9015536a92b8c84f58d1cafdc5f799f04c774a4c
Alexander Marunich <alexander.marunich.89@gmail.com>
2022-01-22 22:35:00 +0300

    feat: adaptive site

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,40 +1,19 @@
-const requirements = `Total 110/100.
+"use strict";
 
-1.Верстка валидная + 10
-2.Вёрстка семантическая +20
-    В коде странице присутствуют следующие элементы :
-        -<header>, <main>, <footer> +2
-        -шесть элементов <section> (по количеству секций) +2
-        -только один заголовок <h1> +2
-        -пять заголовков <h2> (количество секций минус одна, у которой заголовок <h1>) +2
-        -один элемент <nav> (панель навигации) +2
-        -два списка ul > li > a (панель навигации, ссылки на соцсети) +2
-        -десять кнопок <button> +2
-        -два инпута: <input type="email"> и <input type="tel"> +2
-        -один элемент <textarea> +2
-        -три атрибута placeholder +2
-3.Вёрстка соответствует макету +48
-    -блок <header> +6
-    -секция hero +6
-    -секция skills +6
-    -секция portfolio +6
-    -секция video +6
-    -секция price +6
-    -секция contacts +6
-    -блок <footer> +6
-4.Требования к css + 12
-    -для построения сетки используются флексы или гриды +2
-    -при уменьшении масштаба страницы браузера вёрстка -размещается по центру, а не сдвигается в сторону +2
-    -фоновый цвет тянется на всю ширину страницы +2
-    -иконки добавлены в формате .svg. SVG может быть добавлен -любым способом. Обращаем внимание на формат, а не на -способ добавления +2
-    -изображения добавлены в формате .jpg +2
-    -есть favicon +2
-5.Интерактивность, реализуемая через css +20
-    -плавная прокрутка по якорям +5
-    -ссылки в футере ведут на гитхаб автора проекта и на страницу курса https://rs.school/js-stage0/ +5
-    -интерактивность включает в себя не только изменение внешнего вида курсора, например, при помощи свойства cursor: pointer, но и другие визуальные эффекты, например, изменение цвета фона или цвета шрифта. Если в макете указаны стили при наведении и клике, для элемента указываем эти стили. Если в макете стили не указаны, реализуете их по своему усмотрению, руководствуясь общим стилем макета +5
-    -обязательное требование к интерактивности: плавное -изменение внешнего вида элемента при наведении и клике не влияющее на соседние элементы +5
+const hamburgerBtn = document.querySelector("#hamburger");
+const navList = document.querySelector(".nav-list");
+const transparentLayer = document.querySelector(".transparent-layer");
 
+console.log(navList);
 
-`;
-console.log(requirements);
+hamburgerBtn.addEventListener("click", function () {
+  hamburgerBtn.classList.toggle("is-active");
+  navList.classList.toggle("open");
+  transparentLayer.classList.toggle("visible-off");
+});
+
+navList.addEventListener("click", function () {
+  navList.classList.remove("open");
+  hamburgerBtn.classList.remove("is-active");
+  transparentLayer.classList.add("visible-off");
+});
